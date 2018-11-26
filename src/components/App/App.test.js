42f0605bb4ad19e84d08dc9fa21d5e794697e029
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-11-26 16:03:11 +0100

    restructure app

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,3 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+it('shows a Comment Box', () => {
+  // setup
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+
+  // test
+  // console.log(div.innerHTML);
+  expect(div.innerHTML).toContain('<div>Comment Box</div>');
+
+  // cleanup
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+
+it('shows a Comment List', () => {
+
+});
