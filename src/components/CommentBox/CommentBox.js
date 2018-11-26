8eaca86b87d4b40725764cee99b48b06306c95cb
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-11-26 17:01:42 +0100

    presentational comment box

--- src/components/CommentBox/CommentBox.js
+++ src/components/CommentBox/CommentBox.js
@@ -1,3 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => <div>Comment Box</div>;
+class CommentBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comment: ''
+    };
+  }
+  render() {
+    return (
+      <form>
+        <h4>Add a comment</h4>
+        <textarea />
+        <div>
+          <button>Submit comment</button>
+        </div>
+      </form>
+    )
+  }
+};
+
+export default CommentBox;
