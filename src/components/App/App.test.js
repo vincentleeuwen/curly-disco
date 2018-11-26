4b777680c6ca5c72942d38649b009478cbb1b07c
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-11-26 16:51:51 +0100

    use abolute imports

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import App from './App';
-import CommentBox from '../CommentBox';
-import CommentList from '../CommentList';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
-it('shows a Comment Box', () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper.find(CommentBox).length).toEqual(1);
-});
+describe('App test', () => {
+  it('shows a Comment Box', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(CommentBox).length).toEqual(1);
+  });
 
-it('shows a Comment List', () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper.find(CommentList).length).toEqual(1);
+  it('shows a Comment List', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(CommentList).length).toEqual(1);
+  });
 });
