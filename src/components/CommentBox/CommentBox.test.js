59b81988e991434598c6882e5edbfd6d19f6fcbb
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-11-27 12:53:11 +0100

    update tests

--- src/components/CommentBox/CommentBox.test.js
+++ src/components/CommentBox/CommentBox.test.js
@@ -4,16 +4,28 @@ import CommentBox from 'components/CommentBox';
 
 describe('CommentBox tests', () => {
   let wrapper;
+  
   beforeEach(() => {
     wrapper = mount(<CommentBox />);
-  })
+  });
+  
+  afterEach(() => {
+    wrapper.unmount();
+  });
 
   it('has a text area and a button', () => {
     expect(wrapper.find('textarea').length).toEqual(1);
     expect(wrapper.find('button').length).toEqual(1);
   });
 
-  afterEach(() => {
-    wrapper.unmount();
-  })
+  it('has a textarea that users can type in', () => {
+    const newComment = 'foo';
+    // simulate change event on our textbox
+    wrapper.find('textarea').simulate('change', { target: { value: newComment }});
+    // force an update because setState() is async
+    wrapper.update();
+    // assert that the state is updated
+    expect(wrapper.state().comment).toEqual(newComment);
+    expect(wrapper.find('textarea').prop('value')).toEqual(newComment);
+  });
 });
