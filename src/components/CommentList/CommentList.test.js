fe6056557f97f71b64e31a0bc827d7b8d8076488
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-12-06 11:46:24 +0100

    add comment visibility test

--- src/components/CommentList/CommentList.test.js
+++ src/components/CommentList/CommentList.test.js
@@ -3,16 +3,21 @@ import { mount } from 'enzyme';
 import Root from 'Root';
 import CommentList from 'components/CommentList';
 
-let wrapper;
-beforeEach(() => {
-  const initialState = {comments: ['first comment', 'second comment', 'third comment']};
-  wrapper = mount(<Root initialState={initialState}><CommentList /></Root>);
-})
 
-it('Shows one "li" element per comment', () => {
-  expect(wrapper.find('li').length).toEqual(3);
+describe('CommentList tests', () => {
+  let wrapper;
+  beforeEach(() => {
+    const initialState = {comments: ['first comment', 'second comment', 'third comment']};
+    wrapper = mount(<Root initialState={initialState}><CommentList /></Root>);
+  })
+
+  it('Shows one "li" element per comment', () => {
+    expect(wrapper.find('li').length).toEqual(3);
+  });
+
+  it('text from each comment is visible', () => {
+    expect(wrapper.render().text()).toContain('first comment');
+    expect(wrapper.render().text()).toContain('second comment');
+    expect(wrapper.render().text()).toContain('third comment');
+  });
 });
-
-// it('text from each comment is visible', () => {
-  
-// });
