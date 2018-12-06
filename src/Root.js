a3d88b42b9d95b26afd7a02232385bfef98c2c10
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-12-06 12:22:06 +0100

    add reduxPromise to store middleware

--- src/Root.js
+++ src/Root.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import PropTypes from 'prop-types';
+import reduxPromise from 'redux-promise';
 import reducers from 'reducers';
 
-const Root = ({ children, initialState }) => (
-  <Provider store={createStore(reducers, initialState)}>
-    { children }
-  </Provider>
-);
+const Root = ({ children, initialState }) => {
+  const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(reduxPromise)
+  );
+  return <Provider store={store}>{children}</Provider>
+};
 
 Root.propTypes = {
   initialState: PropTypes.object.isRequired,
