d99681e29cd12727298e5d5a2febb5a82cee1c28
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-12-07 13:45:25 +0100

    render basic header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,29 @@
-import React, { Fragment } from 'react';
-import { Route } from 'react-router-dom';
+import React, { Fragment, Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 import CommentBox from '../CommentBox';
 import CommentList from '../CommentList';
 
-export default () => (
-  <Fragment>
-    <Route path="/post" component={CommentBox} />
-    <Route path="/" component={CommentList} exact />
-  </Fragment>
-);
+export default class App extends Component {
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post a comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    )
+
+  }
+  render() {
+    return (
+      <Fragment>
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" component={CommentList} exact />
+      </Fragment>
+    );
+  }
+}
