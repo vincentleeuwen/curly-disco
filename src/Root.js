cc71e515c28df74b8671a765c9dae259e20c0686
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-12-12 14:39:08 +0100

    add custom middleware for promise resolution

--- src/Root.js
+++ src/Root.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import PropTypes from 'prop-types';
-import reduxPromise from 'redux-promise';
+// import reduxPromise from 'redux-promise';
 import reducers from 'reducers';
+import customReduxPromise from 'middlewares/async';
 
 const Root = ({ children, initialState }) => {
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(reduxPromise)
+    applyMiddleware(customReduxPromise)
   );
   return <Provider store={store}>{children}</Provider>
 };
---
+++ src/middlewares/async.js
@@ -0,0 +1,26 @@
+// crazy middleware syntax
+export default ({ dispatch }) => next => action => {
+  // check to see if the action has a promise on its 'payload' property
+  
+  // if it doesn't, send action onto next middleware
+  if (!action.payload || !action.payload.then) {
+    return next(action);
+  }
+
+  // if it does, then wait for it to resolve (get the data), and then create
+  // a new action with that data and dispatch it.
+  action.payload.then(response => {
+    const newAction = { ...action, payload: response };
+    dispatch(newAction);
+  });
+}
+
+
+// boilerlate:
+// export default function({ dispatch }) {
+//   return function(next) {
+//     return function(action) {
+
+//     }
+//   }
+// }
